68e60b70338e4f520fcf1f5381161385521ed3f7
Orkhan11 <o.guliyev1@gmail.com>
2021-04-17 20:37:12 +0400

    message

--- ProjectOne/src/Main.java
+++ ProjectOne/src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println("hello world");
-        
+        System.out.println("jkjk");
 
+        main(9);
 
+    }
 
-
+    static void main(int y){
+        System.out.println("mmm");
     }
 }
